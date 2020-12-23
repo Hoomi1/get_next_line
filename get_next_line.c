658575c0a89eb300aaee6a710827def6cd47fad3
Cresswell Yuuki <cyuuki@oa-i5.msk.21-school.ru>
2020-12-23 21:04:30 +0300

    school

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cyuuki <cyuuki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 16:33:06 by cyuuki            #+#    #+#             */
-/*   Updated: 2020/12/22 20:27:39 by cyuuki           ###   ########.fr       */
+/*   Updated: 2020/12/23 20:56:52 by cyuuki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,46 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-static int add_check(char **line, char *buffer, char *poin, char **cache)
+static int add_check(char **line, char *buffer, char **cache)
 {
 	char *tmp;
+	char *point;
+	int i;
 
-	if(!buffer)
+	i = 0;
+	if (!buffer)
 		return (0);
-	tmp = NULL;
-		if ((poin = ft_strchr(buffer, '\n')))
+	if((point = ft_strchr(buffer, '\n')))
+	{
+		*point = '\0';
+		point++;
+		if(*point != '\0')
+		*cache = ft_strdup(point);
+		//printf("123");
+	}
+	tmp = ft_strjoin(*line, buffer);
+	//printf("111");
+	free(*line);
+	*line = tmp;
+	//free(tmp);
+
+	/*tmp = NULL;
+		if ((point = ft_strchr(buffer, '\n')))
 		{
-			*poin = '\0';
-			poin++;
-			tmp = ft_strdup(poin);
-		}
-	free(*cache);
-	*cache = tmp;
-	tmp = *line;
-	*line = ft_strjoin(*line, buffer);
-	free(tmp);
+			*point = '\0';
+		}*/
+//	free(*cache);
+	//*cache = tmp;
+	//tmp = *line;
+//	*line = ft_strjoin(*line, buffer);
+	//free(tmp);
 	//free(*line);
 	// *line = ft_strjoin(*line, *cache);
 	// free(*cache);
-	if(*line == '\0')
+	if(point)
 		return (1);
 	else
 		return (0);
-
-
 }
 
 int get_next_line(int fd, char **line)
@@ -49,32 +62,35 @@ int get_next_line(int fd, char **line)
 	int add;
 	char *poin;
 	static char *cache;
-	int flag;
+	char *tmp;
+	int i;
 
-	flag = 1;
+	i = 2;
 	if ((fd < 0) || (line == NULL) || (BUFFER_SIZE < 1) || !(*line = (char *)malloc(1)))
 		return (-1);
 	**line = '\0';
 	while ((add = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[add] = '\0';
-		add = add_check(line, buffer, poin, &cache);
-		/*if ((poin = ft_strchr(buffer, '\n')))
-		{
-			*poin = '\0';
-			poin++;
-			flag = 0;
-		}
-		*line = ft_strjoin(*line, buffer);*/
-		//free(*line);
+		//printf("line\n%s\n", *line);
+		//printf("buffer\n%s\n", buffer);
+		add = add_check(line, buffer, &cache);
+		if(add == 1)
+			break;
 	}
-	if(add == -1)
+	//printf("111");
+	//printf("%s\n", tmp);
+	/*if(add == -1)
 	return (-1);
 
-	return (0);
+	return (0);*/
+	if(add == 1)
+		return (1);
+	else
+		return (0);
 }
-/*
-int main(void)
+
+/*int main(void)
 {
 	char *string;
 	int fd;
@@ -82,6 +98,8 @@ int main(void)
 	fd = open("tnt.txt", O_RDONLY);
 	get_next_line(fd, &string);
 	printf("%s\n", string);
+	get_next_line(fd, &string);
+	printf("%s\n", string);
 	return (0);
-}
-*/
+}*/
+
